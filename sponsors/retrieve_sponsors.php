b4eddd07e2534705901cc377ff2ec34f94ff46f6
cpabbot <cpabbot1@gmail.com>
2018-12-04 10:19:56 -0500

    in process: mobile responsive, action menu at top of sponsors page

--- sponsors/retrieve_sponsors.php
+++ sponsors/retrieve_sponsors.php
@@ -95,8 +95,30 @@ if(mysqli_num_rows($result) != 0) {
             echo "$$total_donated";
         ?></span>
             <span style="display:inline-block;width:20px"></span>
+            <!-- email -->
         <a target="_blank" href="http://compose.mail.yahoo.com/?to=TO&subject=TeamParagon571Map&body="><img src="../img/email.svg" alt="email"></a>
+            <!-- letter -->
         <a target="_blank" href="letter.php?sponsorid=<?php echo $sponsor_ID; ?>"><img src="../img/letter.svg" alt="letter"></a>
+            
+        <div class="extra-sponsor-quicklinks">
+            <!-- archive -->
+        <a id="archive-sponsor" target="_top" href="../toggle_archive.php?type=sponsors&id=<?php echo $sponsor_ID; ?>">
+            <?php
+            if($_SESSION['sort'] == "archived") {
+                echo "<img src='../img/edit.svg' alt='archive'>";
+            }
+            else {
+                echo "<img src='../img/done.svg' alt='archive'>";
+            }
+            ?>
+        </a>
+            <!-- delete -->
+        <?php
+            if($_SESSION['sort'] == "archived") {
+                echo "<a id='delete-sponsor' target='_top' href='delete_sponsor.php?id=$sponsor_ID?>'><img src='../img/close-dark.svg' alt='delete'></a>";
+            }
+        ?>
+        </div>
         </div>
         
         <div class="beneath-card">
@@ -131,11 +153,26 @@ else {
 <input type="hidden" name="sql" value="<?php echo $sql ?>">
 
 <div class="floaty-btn floaty-btn-download floaty-btn-download--fixed" onclick="document.forms['download-form'].submit();">
-  <span class="floaty-btn-label">Export</span>
+    <span class="floaty-btn-label">Export</span>
     <img src="../img/download-light.svg" class="floaty-btn-icon--download absolute-center">
 </div>
 
 </form>
+<!----------------------------------->
+    
+<!------ BETA ACTION MENU ----------->
+<div class="main-action-menu">
+    <!-- download -->
+        <div class="floaty-btn floaty-btn-download" onclick="document.forms['download-form'].submit();">
+            <span class="floaty-btn-label">Export</span>
+            <img src="../img/download-light.svg" class="floaty-btn-icon--download absolute-center">
+        </div>
+    <!-- new sponsor -->
+        <div class="floaty-list-item" id="new-sponsor">
+            <span class="floaty-list-item-label">New Sponsor</span>
+            <img src="../img/new-sponsor.svg" class="floaty-btn-icon absolute-center icon">
+        </div>
+</div>
 <!----------------------------------->
 
     
